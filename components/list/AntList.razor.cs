5b73915b2f0cf8db6f66fa994ac467b599b10f5a
ElderJames <shunjiey@hotmail.com>
2020-09-15 19:24:45 +0800

    refactor(module: list): allow to setup each list item

--- components/list/AntList.razor.cs
+++ components/list/AntList.razor.cs
@@ -1,10 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
-using OneOf;
-using System;
-using System.Collections.Generic;
-using System.Drawing;
-using System.Text;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
 
 namespace AntDesign
 {
@@ -23,8 +19,6 @@ namespace AntDesign
     public partial class AntList<TItem> : AntDomComponentBase
     {
         public string PrefixName { get; set; } = "ant-list";
-
-        [Parameter] public RenderFragment<TItem> Item { get; set; }
 
         [Parameter] public IEnumerable<TItem> DataSource { get; set; }
 
@@ -46,19 +40,13 @@ namespace AntDesign
 
         [Parameter] public bool Split { get; set; } = true;
 
-        [Parameter] public string ClassName { get; set; }
-
         [Parameter] public EventCallback<TItem> OnItemClick { get; set; }
 
         [Parameter] public ListGridType Grid { get; set; }
 
-        [Parameter] public List<RenderFragment> Actions { get; set; } = new List<RenderFragment>();
-
-        [Parameter] public RenderFragment Extra { get; set; }
-
         [Parameter] public PaginationOptions Pagination { get; set; }
 
-        private static readonly EventCallbackFactory CallbackFactory = new EventCallbackFactory();
+        [Parameter] public RenderFragment<TItem> ChildContent { get; set; }
 
         private bool IsSomethingAfterLastItem
         {
@@ -101,10 +89,9 @@ namespace AntDesign
 
             ClassMapper.Clear()
                 .Add(PrefixName)
-                .Add(ClassName)
                 .If($"{PrefixName}-split", () => Split)
                 .If($"{PrefixName}-bordered", () => Bordered)
-                .If($"{PrefixName}-{sizeCls}", () => !string.IsNullOrEmpty(sizeCls))
+                .GetIf(() => $"{PrefixName}-{sizeCls}", () => !string.IsNullOrEmpty(sizeCls))
                 .If($"{PrefixName}-vertical", () => ItemLayout == AntDirectionVHType.Vertical)
                 .If($"{PrefixName}-loading", () => (Loading))
                 .If($"{PrefixName}-grid", () => Grid != null)
---
+++ components/list/ListItemMeta.razor.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace AntDesign
+{
+    public partial class ListItemMeta : AntDomComponentBase
+    {
+        public string PrefixName { get; set; } = "ant-list-item-meta";
+
+        [Parameter] public RenderFragment Title { get; set; }
+
+        [Parameter] public OneOf.OneOf<string, RenderFragment> Avatar { get; set; }
+
+        [Parameter] public string Description { get; set; }
+
+        [CascadingParameter] public ListItem ListItem { get; set; }
+
+        protected override void OnInitialized()
+        {
+            base.OnInitialized();
+            SetClassMap();
+        }
+
+        protected void SetClassMap()
+        {
+            ClassMapper.Clear()
+                .Add(PrefixName);
+        }
+    }
+}
